970c43d3478dfddfe98051f0e4b91a84175858a0
Rodrigo Soto Cid <rodrigo.soto.web82@gmail.com>
2024-08-09 13:31:49 +0200

    Agregando formato a las transacciones

---
+++ MAUI.ProsperDaily/Converters/AmountToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace MAUI.ProsperDaily.Converters
+{
+    public class AmountToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var isIncome = ((Label)parameter).Text;
+            var amount = (decimal)value;
+
+            if (isIncome == "True")
+            {
+                return Colors.Green;
+            }
+            else
+            {
+                return Colors.Red;
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
